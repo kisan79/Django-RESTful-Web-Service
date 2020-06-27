982452f5b9b89807561a6f275f55b97bd8a6d745
kisan79 <kumar.kisan79@gmail.com>
2020-06-27 08:40:03 +0530

    Functionality for For Giving a HttpResponse which give JSON-Response

--- DjangoProvider/DjangoProvider/urls.py
+++ DjangoProvider/DjangoProvider/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from provider.views import htmlResponseView
+from provider.views import JSONResponseView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('html/',htmlResponseView)
+    path('html/',htmlResponseView),
+    path('json/',JSONResponseView),
 ]
--- DjangoProvider/provider/views.py
+++ DjangoProvider/provider/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 # Creating function-Based-View For Giving a HttpResponse which taking Html-Response
 def htmlResponseView(request):
@@ -13,3 +14,19 @@ def htmlResponseView(request):
     }
     htmlRes='<h1 style="color:darkcyan;">ename:{}<br>Age:{}<br>empno:{}<br>isjoined ?{}<br>Stream:{}<br>Join-date:{}</h1>'.format(emp_details['Name'],emp_details['Age'],emp_details['Id'],emp_details['isJoined'],emp_details['Stream'],emp_details['Join-date'])
     return HttpResponse(htmlRes)
+
+# Creating function-Based-View For Giving a HttpResponse which give JSON-Response
+
+def JSONResponseView(request):
+    emp_details={
+        'Name':'Kisan',
+        'Age':23,
+        'Id':101,
+        'isJoined':False,
+        'Stream':'Developer',
+        'Join-date':'27-06-2020'
+    }
+    #Converting our Python dict object
+    JSONRes=json.dumps(emp_details,indent=0)
+    print(JSONRes)
+    return HttpResponse(JSONRes)
